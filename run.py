41b3a47211920fbb1d7d09b2069a1cd0e85f85b3
Ratiem <ratidzo.makoni@gmail.com>
2022-11-16 21:48:24 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+""" 
+A simple Battleship game
+"""
+
+class ShipBoard:
+    """
+    Main Board class
+    """
+
+    def __init__(self, board):
+        self.board = board
+
+    def get_letters_to_numbers():
+        """
+        For every Letter that will be declared, it will be equivalent to a number.
+        """
+        letters_to_numbers = ["A": 0]
+
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(row))
