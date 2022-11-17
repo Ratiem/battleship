3456833cb32b46f5b5dca71bbd8f2438134d115c
Ratiem <ratidzo.makoni@gmail.com>
2022-11-17 00:58:40 +0000

    Create new game function to run all program functions

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import random
 
-""" 
-A simple Battleship game
 """
+A simple Battleship game of a single user playing against computer board.
+"""
+
 
 class ShipBoard:
     """
@@ -14,7 +15,8 @@ class ShipBoard:
 
     def get_letters_to_numbers():
         """
-        For every Letter that will be declared, it will be equivalent to a number.
+        For every Letter that will be declared,
+        it will be equivalent to a number.
         """
         letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
         return letters_to_numbers
@@ -27,3 +29,75 @@ class ShipBoard:
         print("-" * 50)
         print("Welcome to the Battleships Game!!")
         print("-" * 50)
+
+
+class Battleship:
+    def __init__(self, board):
+        self.board = board
+
+    def add_ships(self):
+        for i in range(5):
+            self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            while self.board[self.x_row][self.y_column] == "X":
+                self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            self.board[self.x_row][self.y_column] = "X"
+        return self.board
+
+    def populate_sunk_ships(self):
+        sunk_ships = 0
+        for row in self.board:
+            for column in row:
+                if column == "X":
+                    sunk_ships += 1
+        return sunk_ships
+
+    def guess(self):
+        try:
+            x_row = input("Enter the row number of the ship: ")
+            while x_row not in '12345678':
+                print("Not a valid choice, please enter a valid row number")
+            x_row = input("Enter the row number of the ship: ")
+
+            y_column = input("Enter the column letter of the ship ").upper()
+            while y_column not in 'ABCDEFGH':
+                print("Not a valid choice, please enter a valid column letter")
+            y_column = input("Enter the column letter of the ship ").upper()
+            return int(x_row) - 1, ShipBoard.get_letters_to_numbers()[y_column]
+        except ValueError and KeyError:
+            print("Not a valid input")
+            return self.guess
+
+
+def new_game():
+    computer_board = ShipBoard([[" "] * 8 for i in range(8)])
+    user_board = ShipBoard([[" "] * 8 for i in range(8)])
+    Battleship.add_ships(computer_board)
+
+    turns = 10
+    while turns > 0:
+        ShipBoard.print_board(user_board)
+        user_x_row, user_y_column = Battleship.guess(object)
+        while user_board.board[user_x_row][user_y_column] == "-" or user_board.board[user_x_row][user_y_column] == "X":
+            print("You have already guessed this one")
+        user_x_row, user_y_column = Battleship.guess(object)
+        if computer_board.board[user_x_row][user_y_column] == "X":
+            print("You sunk 1 of my battleship!")
+        user_board.board[user_x_row][user_y_column] = "X"
+    else:
+        print("You missed my battleship!")
+        user_board.board[user_x_row][user_y_column] = "-"
+    if Battleship.populate_sunk_ships(user_board) == 5:
+        print("You hit all 5 battleships!")
+        break
+
+    else:
+    turns -= 1
+    print(f"You have {turns} turns left")
+    if turns == 0:
+        print("Sorry you have run out of turns")
+        ShipBoard.print_board(user_board)
+        break
+
+    if __name__ == '__main__':
+        new_game()
+
